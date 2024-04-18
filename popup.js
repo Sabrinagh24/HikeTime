383d7dac2fece4ade6f90a8233acfae7c537856d
mahieddinee <166138650+mahieddinee@users.noreply.github.com>
2024-04-18 13:28:21 +0200

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,7 @@
+function openDeletePopup() {
+    document.getElementById("supprimer").style.display = "block";
+} //mettre dans le button qui ouvre le pop up//
+  
+function closeDeletePopup() {
+    document.getElementById("supprimer").style.display = "none";
+}
